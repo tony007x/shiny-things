45e5df6503ec1829a60413733f242b3dd0393512
tony007x <aekanan2004@gmail.com>
2024-12-02 21:47:21 +0700

    add wretch and data-sender

--- src/routes/authorization/+page.svelte
+++ src/routes/authorization/+page.svelte
@@ -4,7 +4,23 @@
     import { Mail, Lock, Github, Facebook, User } from "lucide-svelte";
 
     let isRegister: boolean = false;
+
     let username: string;
+    let email: string;
+    let password: string;
+    let confirmpassword: string;
+
+    const login = () => {
+        console.log(username);
+        console.log(password);
+    };
+
+    const register = () => {
+        console.log(username);
+        console.log(email);
+        console.log(password);
+        console.log(confirmpassword);
+    };
 </script>
 
 <div
@@ -19,15 +35,13 @@
             <img src="/logo.png" alt="Logo" class="w-70" />
 
             <!-- Form container -->
-            <form
-                class="flex flex-col w-full items-center gap-6"
-            >
+            <form class="flex flex-col w-full items-center gap-6">
                 <h1 class="font-bold text-5xl text-center">
                     {isRegister ? "REGISTER" : "LOG IN"}
                 </h1>
 
                 <!-- Username Input with Icon -->
-                <div class="relative w-full max-w-xs ">
+                <div class="relative w-full max-w-xs">
                     <Input
                         type="text"
                         placeholder="USERNAME"
@@ -48,6 +62,7 @@
                             placeholder="FULL NAME"
                             class="pl-10 w-full shadow-md"
                             required
+                            bind:value={email}
                         />
                         <Mail
                             class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -62,6 +77,7 @@
                         placeholder="PASSWORD"
                         class="pl-10 w-full shadow-md"
                         required
+                        bind:value={password}
                     />
                     <Lock
                         class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -76,6 +92,7 @@
                             placeholder="CONFIRM PASSWORD"
                             class="pl-10 w-full shadow-md"
                             required
+                            bind:value={confirmpassword}
                         />
                         <Lock
                             class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
@@ -85,6 +102,7 @@
 
                 <!-- Submit Button -->
                 <button
+                    on:click={() => (isRegister ? register() : login())}
                     type="submit"
                     class="p-2 w-1/2 rounded-2xl bg-[#40A2E3] text-white hover:bg-[#3280b4] duration-300 shadow-lg"
                 >
@@ -94,8 +112,8 @@
 
             <!-- Line -->
             <div class="flex flex-col justify-center text-center w-full gap-3">
-                <span class="border-b-2 border-black w-full mt-4" ></span>
-                <h1> or <br> Login via Oauth</h1>
+                <span class="border-b-2 border-black w-full mt-4"></span>
+                <h1>or <br /> Login via Oauth</h1>
                 <div class="flex w-full justify-around p-3">
                     <button
                         class="border rounded-full p-1 hover:bg-gray-500 duration-300"
@@ -120,55 +138,61 @@
 
     <!-- Right section with a background color -->
     <div
-        class="flex flex-col w-1/3 bg-[#40A2E3] relative justify-center items-center gap-10"
+        class="flex flex-col w-1/3 bg-[#40A2E3] relative justify-center items-center gap-10 "
     >
-        <img src="/triangle.png" alt="" class="absolute top-5 left-10" />
+        <img src="/triangle.png" alt="" class="absolute top-5 left-10 " />
         <img
             src="/triangle.png"
             alt=""
-            class="absolute right-14 top-[100px] rotate-45 w-16"
+            class="absolute right-14 top-[100px] rotate-45 w-16 "
         />
 
         <img
             src="/triangle.png"
             alt=""
-            class="absolute right-[100px] bottom-[20px] rotate-90 w-25"
+            class="absolute right-[100px] bottom-[20px] rotate-90 w-25 "
         />
 
-        <img src="/circle.png" alt="" class="absolute left-4 top-1/3" />
+        <img src="/circle.png" alt="" class="absolute left-4 top-1/3 " />
         <img
             src="/circle.png"
             alt=""
-            class="absolute right-14 top-[550px] w-14"
+            class="absolute right-14 top-[600px] w-14"
         />
 
         <img
             src="/Rectangle.png"
             alt=""
-            class="absolute right-0 top-[280px] w-20"
+            class="absolute right-0 top-[280px] w-20 "
         />
 
         <img
             src="/Rectangle.png"
             alt=""
-            class="absolute left-[50px] bottom-[120px] rotate-45 w-16"
+            class="absolute left-[50px] bottom-[120px] rotate-45 w-16 z-10"
         />
 
         <h1 class="text-5xl font-bold text-white text-center">
-            {!isRegister? "New Here?" : "Have an account?"}
+            {!isRegister ? "New Here?" : "Have an account?"}
         </h1>
         <span class="text-center text-white">
-            {#if !isRegister }
+            {#if !isRegister}
                 Enter your personal details <br /> and start journey with us.
             {:else}
-                To keep connected with us please. <br/>login with your personal info
+                To keep connected with us please. <br />login with your personal
+                info
             {/if}
-
         </span>
 
         <button
             class="p-2 w-[200px] rounded-2xl bg-[#D9D9D9] hover:bg-[#bebebe] shadow-md duration-300"
-            on:click={() => (isRegister = !isRegister)}
+            on:click={() => {
+                isRegister = !isRegister;
+                username = ''
+                email = ''
+                password = ''
+                confirmpassword = ''
+            }}
         >
             {isRegister ? "LOGIN" : "REGISTER"}
         </button>

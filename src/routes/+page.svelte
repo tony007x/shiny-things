7a2a51a3c452a3b22fd0ab06107a387c5a8f8eb2
tony007x <aekanan2004@gmail.com>
2024-12-02 15:03:14 +0700

    setup-svelte-tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-2xl">Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>

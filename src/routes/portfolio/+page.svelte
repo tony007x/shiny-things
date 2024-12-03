952ef839d7c00afcbe9b3d066d9548c7d037d5c0
Boby <boblaveyou@gmail.com>
2024-12-03 14:40:28 +0700

    portfolio box in portfolio page

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
     import Navbar from "../(components)/navbar.svelte";
+    import PortfolioBox from "./(component)/PortfolioBox.svelte";
 
 </script>
+
+<div class="flex flex-col w-full h-screen bg-[#FFF6E9]">
+    <PortfolioBox/>
+    <PortfolioBox/>
+    <PortfolioBox/>
+    <PortfolioBox/>
+</div>

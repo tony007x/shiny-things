1c1061b720ef960f002f2bae8098e9a9722557a6
tony007x <aekanan2004@gmail.com>
2024-12-03 13:38:06 +0700

    homepage-post-createpost-sidebar

---
+++ src/routes/home/(component)/postelement.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import * as Avatar from "$lib/components/ui/avatar/index.js";
+    import { Badge } from "$lib/components/ui/badge/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    //@ts-ignore
+    import { Settings2, ThumbsUp, MessageCircle } from 'lucide-svelte';
+
+</script>
+
+<div
+    class="flex flex-col w-full min-h-[250px] h-fit border  gap-3 p-3 bg-[#40A2E3] rounded-2xl shadow-md"
+>
+    <div class="flex  gap-3 items-center">
+        <!-- top -->
+        <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+        </Avatar.Root>
+
+        <div class="flex w-full  border-[red] justify-between">
+            <div class="text-white">
+                <h1>Name</h1>
+                <div class="flex gap-4 items-center">
+                    <h2>20 Dec 2024</h2>
+                    <Badge
+                        class="flex justify-center items-center w-fit h-fit  bg-gray-500 text-white rounded-lg px-4 py-0"
+                        >Tags</Badge
+                    >
+                </div>
+            </div>
+            <div class="flex flex-col  items-end justify-between">
+                <Settings2 color='white'/>
+                <Button class="px-4 h-4 bg-white text-black rounded-full text-[10px] font-bold hover:bg-slate-300" >Apply</Button>
+            </div>
+        </div>
+    </div>
+    <!-- Middle -->
+    <div class="flex border h-full bg-white rounded-md p-3">
+        
+        
+    </div>
+
+    <!-- Bottom -->
+    <div class="flex py-2 gap-4">
+        <Button class="flex px-4 h-6 bg-white text-black rounded-full hover:bg-slate-300 gap-1 items-center" ><ThumbsUp size={16}/>LIKE</Button>
+        <Button class="flex px-4 h-6 bg-white text-black rounded-full hover:bg-slate-300 gap-1 items-center" ><MessageCircle size={16}/>COMMENT</Button>
+    </div>
+</div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
     import { Input } from "$lib/components/ui/input/index.js";
     import Post from "./(component)/createpost.svelte";
+    import Postelement from "./(component)/postelement.svelte";
     import { Badge } from "$lib/components/ui/badge/index.js";
 
 </script>
 
 
-<div class="flex w-full h-[200vh] overflow-y-auto bg-[#FFF6E9]">
+<div class="flex w-full h-screen bg-[#FFF6E9]">
     <!-- left box -->
-    <div class="flex flex-col w-1/3 border-r  p-3">
+    <div class="flex flex-col w-1/3 border-r-2  p-3">
         <Input placeholder="Search. . ."/>
         <div class="grid grid-cols-2 gap-2 w-full p-4 justify-items-center">
             <Badge class="flex justify-center items-center w-fit  bg-gray-500 text-white rounded-lg px-4">Tags</Badge>
@@ -19,12 +20,18 @@
     </div>
 
     <!-- middle box -->
-    <div class="flex w-3/4 ">
-
+    <div class="flex flex-col w-full p-5 gap-4 overflow-scroll">
+        <Postelement/>
+        <Postelement/>
+        <Postelement/>
+        <Postelement/>
+        <Postelement/>
+        <Postelement/>
+        <Postelement/>
     </div>
 
     <!-- right box -->
-    <div class="flex w-1/2 border-l">
+    <div class="flex w-3/4 border-l-2">
         <Post/>
     </div>
 </div>

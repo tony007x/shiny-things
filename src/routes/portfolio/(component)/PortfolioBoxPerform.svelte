d0430bac1a1b036dc4867c310ec9c08b7b64c130
Boby <boblaveyou@gmail.com>
2024-12-03 16:48:42 +0700

    move icon file and page fix

--- src/routes/portfolio/(component)/PortfolioBoxPerform.svelte
+++ src/routes/portfolio/(component)/PortfolioBoxPerform.svelte
@@ -1,16 +1,16 @@
 
 
-<div class="flex flex-row gap-3 p-3">
+<div class="flex flex-row gap-3 p-3 h-full">
     <!-- left text box -->
     <!-- <div class="flex flex-col w-3/4 border-r-2  p-3 bg-[#D9D9D9]"> -->
-    <div class="flex flex-col w-3/4 min-h-[250px] h-fit border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
+    <div class="flex flex-col w-3/4 min-h-[250px] border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo numquam, 
             velit repellat animi recusandae fugiat maxime enim quae veritatis 
             soluta libero maiores odit aliquid voluptatum fugit non deserunt 
             nihil aspernatur?</p>
     </div>
     <!-- right picture box -->
-    <div class="flex flex-col w-1/4 h-fit border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
+    <div class="flex flex-col w-1/4 min-h-[250px] border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
         <img src="https://via.placeholder.com/150" alt="placeholder" class="rounded-3xl">
     </div>
 </div>
--- src/routes/portfolio/(component)/PortfolioBoxSkill.svelte
+++ src/routes/portfolio/(component)/PortfolioBoxSkill.svelte
@@ -3,14 +3,20 @@
 <div class="flex flex-row gap-3 p-3">
     <!-- left text box -->
     <!-- <div class="flex flex-col w-3/4 border-r-2  p-3 bg-[#D9D9D9]"> -->
-    <div class="flex flex-col w-3/4 min-h-[250px] h-fit border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo numquam, 
+    <div class="flex flex-col w-3/4 min-h-[250px] h-auto border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
+        <p>Icon Bro Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo numquam, 
             velit repellat animi recusandae fugiat maxime enim quae veritatis 
             soluta libero maiores odit aliquid voluptatum fugit non deserunt 
             nihil aspernatur?</p>
     </div>
     <!-- right picture box -->
-    <div class="flex flex-col w-1/4 min-h-[250px] h-fit border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
-        <img src="https://via.placeholder.com/150" alt="placeholder" class="rounded-3xl">
+    <div class="grid grid-cols-3 w-1/4 min-h-[250px] h-auto border  gap-3 p-3 rounded-3xl shadow-md bg-[#D9D9D9]">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="Cplusplus" class="rounded-3xl h-24">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="Csharp" class="rounded-3xl h-24">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="Java" class="rounded-3xl h-24">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="JavScript" class="rounded-3xl h-24">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="Python" class="rounded-3xl h-24">
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="TypeScript" class="rounded-3xl h-24">
+        <img src='./ProgramLanguagePic/typescript-original.svg' alt="TypeScript" class="rounded-3xl h-24">
     </div>
 </div>

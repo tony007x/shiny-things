1896d62d5faf932e717473f456f888417d8eb1e1
Boby <boblaveyou@gmail.com>
2024-12-07 17:34:08 +0700

    portfolio page connect backend

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -16,14 +16,38 @@
                 message = c.newtext;
             });
     };
+
+    interface portDatail {
+        context: string;
+        pictureURL: string;
+    }
+
+    let portDatailArr: portDatail[] = [
+        {
+            context: "Loop 1",
+            pictureURL: "https://certifier.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fcertifier%2Fimage%2Fupload%2Fv1709650100%2F51_participation_formal_darkviolet_landscape_ff5e55e81a.png&w=3840&q=100",
+        },
+        {
+            context: "Loop 2",
+            pictureURL: "https://cdn-ghkoj.nitrocdn.com/kjYfdEBKRwdYwvHQyjaYBdTGFpFGjqYW/assets/images/optimized/rev-891ee2d/sertifier.com/blog/wp-content/uploads/2020/10/certificate-text-samples.jpg",
+        },
+        {
+            context: "Loop 3",
+            pictureURL: "https://certifier.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fcertifier%2Fimage%2Fupload%2Fv1709650100%2F51_participation_formal_darkviolet_landscape_ff5e55e81a.png&w=3840&q=100",
+        },
+        {
+            context: "Loop 4",
+            pictureURL: "https://cdn-ghkoj.nitrocdn.com/kjYfdEBKRwdYwvHQyjaYBdTGFpFGjqYW/assets/images/optimized/rev-891ee2d/sertifier.com/blog/wp-content/uploads/2020/10/certificate-text-samples.jpg",
+        },
+    ];
 </script>
 
 <div class="flex flex-col w-full bg-[#FFF6E9]">
-    <!-- <PortfolioBoxPerform/>
-    <PortfolioBoxPerform/>
-    <PortfolioBoxPerform/>
-    <PortfolioBoxPerform/>
-    <PortfolioBoxSkill/> -->
+    {#each portDatailArr as detailEach}
+        <PortfolioBoxPerform detail = {detailEach}/>
+    {/each}
+    <!-- <PortfolioBoxPerform/> -->
+    <PortfolioBoxSkill/>
     <h1>Hello {message}</h1>
 
     <input type="text" bind:value={text} class="bg-white" />
